9df9bb77a7631eddfe542ec0d7b2d7f93e7c4c89
dustinquintanilla <portillo0121@gmail.com>
2024-12-03 19:35:30 -0500

    added a switch statement to handle user input

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,20 +1,49 @@
 import java.util.Scanner;
+import java.util.Set;
 
 public class UserInterface {
 
     static Scanner scnr = new Scanner(System.in);
+    static String UserInput = "";
 
     public static void Menu() {
 
         System.out.println("Welcome to the resume builder program!");
         System.out.println("What would you like to do?");
         System.out.println("""
-                1: Create new resume
-                2: Open existing resume
-                3: Exit the program
+                1: [C]reate new resume
+                2: [O]pen existing resume
+                3: E[x]it the program
                 """);
 
+
+        UserInput = scnr.next();
+        Set<String> validInputs = Set.of("C", "O", "x");
         
+        do { 
+
+            UserInput = scnr.next();
+            if (!validInputs.contains(UserInput)) {
+                System.out.println("Enter a valid input!");
+            }
+
+        } while (!validInputs.contains(UserInput));
+
+        switch (UserInput) {
+            case "C" -> {
+                System.out.println("Creating new resume...");
+                // createNewResume();
+            }
+            case "O" -> {
+                System.out.println("Opening existing resume...");
+                // openExistingResume();
+            }
+            case "x" -> {
+                System.out.println("Exiting the program...");
+                // exitProgram();
+            }
+            default -> System.out.println("Invalid option!");
+        }
 
     }
 
